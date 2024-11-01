1060fcc570de3ce8bb4255e7f70f034456b4698c
Chema <chchema26@gmail.com>
2024-11-01 16:39:17 -0600

    Configurado el Controlador para Articles

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ArticleController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,10 +23,18 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+Route::resource('/articles', ArticleController::class)
+        ->only(['index', 'show']);
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    # ARTICLES
+    Route::resource('/dashboard/articles', ArticleController::class)
+            ->except(['index', 'show']);
 });
 
 require __DIR__.'/auth.php';
