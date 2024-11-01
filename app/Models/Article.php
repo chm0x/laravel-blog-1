424fa7e16e100171c6ff83f5db20ee6128f16bf4
Chema <chchema26@gmail.com>
2024-11-01 14:25:01 -0600

    Definiendo los Modelos y sus Relaciones

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,8 +4,40 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Article extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'category_id',
+        'title',
+        'slug',
+        'excerpt',
+        'description',
+        'status'
+    ];
+
+    # Relation between Article and the User
+    # Each article belongs to One User.
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    # Each article belongs to Category
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+
+    # Many to Many Tags, with pivot table
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description'
+    ];
+
+    # One Category has Many Articles.
+    public function articles():HasMany
+    {
+        return $this->hasMany(Article::class);
+    }
+
+    
+
 }
